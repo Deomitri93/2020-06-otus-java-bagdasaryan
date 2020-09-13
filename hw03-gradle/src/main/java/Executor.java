c56453ddc160177f80ff1a66db4293c23f256693
deomitri93 <deomitri93@mail.ru>
2020-09-13 21:09:38 +0400

    First commit

---
+++ hw03-gradle/src/main/java/Executor.java
@@ -0,0 +1,117 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Executor {
+    private static void statisticsMessage(int testsExecutedCounter, int testsPassedCounter) {
+        String statisticsMessage = "|   " + testsPassedCounter + " test" + (testsPassedCounter == 1 ? "" : "s" + " of " + testsExecutedCounter + " passed" + (testsExecutedCounter - testsPassedCounter > 0 ? " (failed " + (testsExecutedCounter - testsPassedCounter) + ")" : "")) + "   |";
+        String statisticsMessageHBorder = " " + new String(new char[statisticsMessage.length() - 2]).replace('\0', '-') + " ";
+        String statisticsMessageMiddlePart = "|" + (new String(new char[statisticsMessage.length() - 2]).replace('\0', ' ')) + "|";
+
+        System.out.println();
+        System.out.println(statisticsMessageHBorder);
+        System.out.println(statisticsMessageMiddlePart);
+        System.out.println(statisticsMessage);
+        System.out.println(statisticsMessageMiddlePart);
+        System.out.println(statisticsMessageHBorder);
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        String testedClassName;
+
+        if (args.length > 0) {
+            testedClassName = args[0];
+        } else {
+            throw new ClassNotFoundException("No class name passed");
+        }
+
+        // determining class for passed tested class name
+        Class<?> clazz;
+        try {
+            clazz = Class.forName(testedClassName);
+        } catch (ClassNotFoundException e) {
+            throw new ClassNotFoundException("No class with name '" + testedClassName + "' was found");
+        }
+
+        // searching default constructor
+        Constructor<?> testedClassConstructor = null;
+        try {
+            testedClassConstructor = clazz.getConstructor();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+
+        // searching for all methods, annotated with @Test, @Before or @After
+        // and adding them to lists: testMethods, beforeMethods and afterMethods respectively
+        List<Method> beforeMethods = new ArrayList<>();
+        List<Method> testMethods = new ArrayList<>();
+        List<Method> afterMethods = new ArrayList<>();
+        for (Method method : clazz.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(Test.class)) {
+                testMethods.add(method);
+            } else {
+                if (method.isAnnotationPresent(Before.class)) {
+                    beforeMethods.add(method);
+                } else {
+                    if (method.isAnnotationPresent(After.class)) {
+                        afterMethods.add(method);
+                    }
+                }
+            }
+        }
+
+        int testsExecutedCounter = 0;
+        int testsPassedCounter = 0;
+
+        // iterating through all methods, annotated with @Test
+        for (Method testMethod : testMethods) {
+            // for every method, annotated with @Test, creating new instance of tested class
+            Object testedClassInstance = null;
+            try {
+                if (testedClassConstructor != null) {
+                    testedClassConstructor.setAccessible(true);
+                } else {
+                    throw new NullPointerException();
+                }
+                testedClassInstance = testedClassConstructor.newInstance();
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+
+            // for every method, annotated with @Test, invoking all methods, annotated with @Before
+            for (Method beforeMethod : beforeMethods) {
+                beforeMethod.setAccessible(true);
+                try {
+                    beforeMethod.invoke(testedClassInstance);
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // invoking method, annotated with @Test
+            testsExecutedCounter++;
+            testMethod.setAccessible(true);
+            try {
+                testMethod.invoke(testedClassInstance);
+                testsPassedCounter++;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            //for every method, annotated with @Test, invoking all methods, annotated with @After
+            for (Method afterMethod : afterMethods) {
+                afterMethod.setAccessible(true);
+                try {
+                    afterMethod.invoke(testedClassInstance);
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        statisticsMessage(testsExecutedCounter, testsPassedCounter);
+    }
+}
---
+++ hw03-gradle/src/main/java/TestedClass.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestedClass {
+    private final int LIST_OF_INTEGERS_SIZE = 15;
+    private final int MATRIX_ROWS_COUNT = 5;
+    private final int MATRIX_COLUMNS_COUNT = 4;
+    private List<Integer> listOfIntegers = null;
+    private float[][] matrix = null;
+    private final int MIN_INTEGER_VALUES = -10;
+    private final int MAX_INTEGER_VALUES = 10;
+    private final float MIN_FLOAT_VALUES = -10.0f;
+    private final float MAX_FLOAT_VALUES = 10.0f;
+
+    @Before
+    public void initListIntegers() {
+        System.out.println("method \"initListOfIntegers()\" executed");
+        listOfIntegers = new ArrayList<>();
+        for (int i = 0; i < LIST_OF_INTEGERS_SIZE; i++) {
+            listOfIntegers.add((int) ((Math.random() - 0.5) * (MAX_INTEGER_VALUES - MIN_INTEGER_VALUES)));
+        }
+    }
+
+    @Before
+    public void initMatrix() {
+        System.out.println("method \"initMatrix()\" executed");
+        matrix = new float[MATRIX_ROWS_COUNT][MATRIX_COLUMNS_COUNT];
+        for (int i = 0; i < MATRIX_ROWS_COUNT; i++) {
+            for (int j = 0; j < MATRIX_COLUMNS_COUNT; j++) {
+                matrix[i][j] = (float) ((Math.random() - 0.5) * (MAX_FLOAT_VALUES - MIN_FLOAT_VALUES));
+            }
+        }
+    }
+
+    @Test
+    private void testListOfIntegersAddingRandomInteger() {
+        System.out.println("method \"testListOfIntegersAddingRandomInteger()\" executed");
+        Integer addedValue = (int) ((Math.random() - 0.5) * (MAX_INTEGER_VALUES - MIN_INTEGER_VALUES));
+        listOfIntegers.add(addedValue);
+    }
+
+    @Test
+    public void testListOfIntegersAddingRandomIntegerToSpecifiedPosition() {
+        System.out.println("method \"testListOfIntegersAddingRandomIntegerToSpecifiedPosition()\" executed");
+        Integer addedValue = (int) ((Math.random() - 0.5) * (MAX_INTEGER_VALUES - MIN_INTEGER_VALUES));
+        listOfIntegers.add(50, addedValue);
+    }
+
+    @Test
+    public void testMatrixTransposing() {
+        System.out.println("method \"testMatrixTransposing()\" executed");
+        float[][] matrixTransposed = new float[matrix[0].length][matrix.length];
+
+        for (int i = 0; i < MATRIX_ROWS_COUNT; i++) {
+            for (int j = 0; j < MATRIX_COLUMNS_COUNT; j++) {
+                matrixTransposed[j][i] = matrix[i][j];
+            }
+        }
+
+        matrix = matrixTransposed;
+    }
+
+    @After
+    public void nullAll() {
+        System.out.println("method \"nullAll()\" executed");
+        System.out.println();
+        listOfIntegers = null;
+        matrix = null;
+    }
+}
