51657ad26391398a119f5c58ec3a4ccbb3149bf4
Deomitri93 <deomitri93@gmail.com>
2020-12-22 11:42:29 +0300

    22.12.2020 (hw15-multithreading defects corrected)

--- hw11-gradle/src/main/java/ru/otus/cache/MyCache.java
+++ hw11-gradle/src/main/java/ru/otus/cache/MyCache.java
@@ -6,30 +6,55 @@ import org.slf4j.LoggerFactory;
 import java.util.*;
 
 public class MyCache<K, V> implements HwCache<K, V> {
+    public class WHMReferenceKey<K> {
+        private final K key;
+
+        public WHMReferenceKey(K key) {
+            this.key = key;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            WHMReferenceKey<?> that = (WHMReferenceKey<?>) o;
+            return key.equals(that.key);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(key);
+        }
+    }
+
     //Надо реализовать эти методы
     private static final Logger logger = LoggerFactory.getLogger(MyCache.class);
-    private final String PUT = "put";
-    private final String REMOVE = "remove";
+    private static final String PUT = "put";
+    private static final String REMOVE = "remove";
     private final Set<HwListener<K, V>> listeners = Collections.newSetFromMap(new WeakHashMap<>());
-    private final Map<K, V> values = new WeakHashMap<>();
+    private final Map<WHMReferenceKey<K>, V> values = new WeakHashMap<>();
 
     @Override
     public void put(K key, V value) {
-        values.put(key, value);
+        values.put(new WHMReferenceKey<>(key), value);
         notifyListeners(key, value, PUT);
     }
 
     @Override
     public void remove(K key) {
-        if(values.containsKey(key)){
-            var value = values.remove(key);
+        if (values.containsKey(new WHMReferenceKey<>(key))) {
+            var value = values.remove(new WHMReferenceKey<>(key));
             notifyListeners(key, value, REMOVE);
         }
     }
 
     @Override
     public V get(K key) {
-        return values.get(key);
+        if (values.containsKey(new WHMReferenceKey<>(key))) {
+            return values.get(new WHMReferenceKey<>(key));
+        }
+
+        return null;
     }
 
     @Override
